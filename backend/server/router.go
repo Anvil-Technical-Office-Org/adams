0124b243559bff17a150ef0f23252463a7240620
stakizawa <stakizawa@anvil.ne.jp>
2022-01-19 17:24:09 +0900

    - CROSの対応 - OpenAPIからTypeScriptの自動型生成コマンド追加

--- backend/server/router.go
+++ backend/server/router.go
@@ -3,18 +3,31 @@ package server
 import (
 	"backend/controllers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func router() (r *gin.Engine) {
 	r = gin.Default()
 
-	// ルーティングサンプル
-	sample := r.Group("/samples")
-	{
-		ctrl := controllers.SampleController{}
-		sample.GET("", ctrl.Index)
-		sample.POST("", ctrl.Create)
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{
+		"http://localhost:3000",
 	}
+	r.Use(cors.New(config))
+
+
+	v1 := r.Group("/v1")
+
+	{
+		// ルーティングサンプル
+		sample := v1.Group("/samples")
+		{
+			ctrl := controllers.SampleController{}
+			sample.GET("", ctrl.Index)
+			sample.POST("", ctrl.Create)
+		}
+	}
+
 	return
 }
